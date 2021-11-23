de290e4e135b0958ec6ab2634424df13d32a2098
lixin yang <2690688423@qq.com>
2021-11-23 21:41:26 +0800

    init syncer

--- internal/pkg/chain/syncer.go
+++ internal/pkg/chain/syncer.go
@@ -3,11 +3,25 @@ package chain
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
-	logging "github.com/ipfs/go-log/v2"
 	"quorum/internal/pkg/nodectx"
 	quorumpb "quorum/internal/pkg/pb"
+
+	logging "github.com/ipfs/go-log/v2"
+)
+
+var syncer_log = logging.Logger("syncer")
+
+var WAIT_BLOCK_TIME_S = 10
+var RETRY_LIMIT = 30
+
+const (
+	SYNCING_FORWARD  = 0
+	SYNCING_BACKWARD = 1
+	SYNC_FAILED      = 2
+	IDLE             = 3
 )
 
 type Syncer struct {
@@ -22,3 +36,16 @@ type Syncer struct {
 	responses        map[string]*quorumpb.ReqBlockResp
 	groupId          string
 }
+
+func (syncer *Syncer) Init(grp *Group, trxMgr *TrxMgr)  {
+	syncer_log.Debug("Init called")
+	syncer.Status = IDLE
+	syncer.group = grp
+	syncer.trxMgr = trxMgr
+	syncer.retryCount = 0
+	syncer.responses = make(map[string]*quorumpb.ReqBlockResp)
+	syncer.groupId = grp.Item.GroupId
+	syncer_log.Infof("<%s> syncer initialed", syncer.groupId)
+}
+
+
